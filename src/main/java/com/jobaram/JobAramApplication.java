cdee00d5d7ce28ee286c3d2b344b1fd92d863de3
giyatto <bluem31@gmail.com>
2018-12-09 22:44:12 +0900

    default setting

---
+++ src/main/java/com/jobaram/JobAramApplication.java
@@ -0,0 +1,12 @@
+package com.jobaram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAramApplication.class, args);
+	}
+}
