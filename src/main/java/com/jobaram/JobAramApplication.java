931a5626b059c816a8da4ff92e839bf138c98c0f
giyatto <bluem31@gmail.com>
2019-02-11 18:58:18 +0900

    sitemesh3 추가

--- src/main/java/com/jobaram/JobAramApplication.java
+++ src/main/java/com/jobaram/JobAramApplication.java
@@ -2,6 +2,11 @@ package com.jobaram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.jobaram.config.SiteMeshFilter;
+
 
 @SpringBootApplication
 public class JobAramApplication {
@@ -9,4 +14,12 @@ public class JobAramApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JobAramApplication.class, args);
 	}
+	
+	@Bean
+	public FilterRegistrationBean siteMeshFilter() {
+		FilterRegistrationBean filter = new FilterRegistrationBean();
+		filter.setFilter(new SiteMeshFilter()); 
+		return filter;
+	}
+	
 }
